1e1a7d20d65c175f67f5fc3746e70d916c17f7a8
Andreas Magnussen <andreas.magnussen@nhn.no>
2025-12-16 10:47:42 +0100

    Miljøvariabel

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    environment = os.getenv("ENVIRONMENT", "")
+    suffix = f" {environment}" if environment else ""
+    return f"<h1>Hello, World!{suffix}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
